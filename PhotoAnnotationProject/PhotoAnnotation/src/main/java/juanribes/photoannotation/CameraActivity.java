5889b7d350ce620d4996fc74490521f4fd1a480d
Juan Ribes <ribes03@yahoo.com>
2013-07-08 20:57:24 +0200

    Empezando

--- PhotoAnnotationProject/PhotoAnnotation/src/main/java/juanribes/photoannotation/CameraActivity.java
+++ PhotoAnnotationProject/PhotoAnnotation/src/main/java/juanribes/photoannotation/CameraActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,14 +20,14 @@ import android.widget.ImageView;
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.main);
-            this.imageView = (ImageView)this.findViewById(R.id.imageView1);
-            Button photoButton = (Button) this.findViewById(R.id.button1);
+            setContentView(R.layout.activity_main);
+            this.imageView = (ImageView)this.findViewById(R.id.imageView);
+            Button photoButton = (Button) this.findViewById(R.id.button);
             photoButton.setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View v) {
-                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(cameraIntent, CAMERA_REQUEST);
                 }
             });
