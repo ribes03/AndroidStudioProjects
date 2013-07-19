b8661b0803aef6a428404d1894db2fe30a95d2f0
Juan Ribes <ribes03@yahoo.com>
2013-07-19 13:29:04 +0200

    Activities

--- PhotoAnnotationProject/PhotoAnnotation/src/main/java/juanribes/photoannotation/CameraActivity.java
+++ PhotoAnnotationProject/PhotoAnnotation/src/main/java/juanribes/photoannotation/CameraActivity.java
@@ -11,6 +11,7 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.view.Menu;
 
 
     public class CameraActivity extends Activity {
